3c1a30a320454cd10f25feb50ab4f39a3a6c03a6
Suraj Vishwakarma <vishwakarmasuraj631@gmail.com>
2021-11-29 10:37:55 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -8,18 +8,28 @@ const App = () => {
   const [NewRow, setNewRow] = useState([]);
   const [NewRow2, setNewRow2] = useState([0,1,2,3,4]);
   const [allRowsAdded, updateAllRows] = useState(0);
-  const [viscosity, setViscosity] = useState([]);
 
   const [IntensificationRatio, setIntensificationRatio] = useState()
+
   const [editFormData, setEditFormData] = useState({
     Injection_Speed: "",
     Fill_Time: "",
     Peak_Inj_Press: "",
     Viscosity: "",
-    Shear_Rate: "",
-    Absolute_Drop_Viscosity: ""
+    Shear_Rate: ""
   })
+
+  const [editFormData2, setEditFormData2] = useState({
+    Injection_Speed2: "",
+    Fill_Time2: "",
+    Peak_Inj_Press2: "",
+    Viscosity2: "",
+    Shear_Rate2: ""
+  })
+
   const [isRowId, setIsRowId] = useState(null)
+
+  // const [isRowId2, setIsRowId2] = useState(null)
 
   const handleEditFormChange = (event) => {
     event.preventDefault();
@@ -33,6 +43,18 @@ const App = () => {
     setEditFormData(newFormData);
   }
 
+  // const handleEditFormChange2 = (event) => {
+  //   event.preventDefault();
+
+  //   const fieldName2 = event.target.getAttribute("name");
+  //   const fieldValue2 = event.target.value;
+
+  //   const newFormData2 = { ...editFormData2 };
+  //   newFormData2[fieldName2] = fieldValue2;
+
+  //   setEditFormData2(newFormData2);
+  // }
+
   const handleEditFormSubmit = (event) => {
       event.preventDefault();
 
@@ -42,16 +64,33 @@ const App = () => {
         Fill_Time: editFormData.Fill_Time,
         Peak_Inj_Press: editFormData.Peak_Inj_Press,
         Viscosity: editFormData.Fill_Time * editFormData.Peak_Inj_Press * IntensificationRatio,
-        Shear_Rate: 1 / editFormData.Fill_Time
+        Shear_Rate: 1 / editFormData.Fill_Time,
       }
+
+      // const editedValue2 = {
+      //   id: isRowId2,
+      //   Injection_Speed2: editFormData2.Injection_Speed2,
+      //   Fill_Time2: editFormData2.Fill_Time2,
+      //   Peak_Inj_Press2: editFormData2.Peak_Inj_Press2,
+      //   Viscosity2: editFormData2.Fill_Time2 * editFormData2.Peak_Inj_Press2 * IntensificationRatio,
+      //   Shear_Rate2: 1 / editFormData2.Fill_Time2,
+      // }
 
       const newValues = [...NewRow2];
 
+      // const newValues2 = [...NewRow];
+
       const index = NewRow2.findIndex((value) => value === isRowId)
+
+      // const index2 = NewRow.findIndex((value) => value === isRowId2)
 
       newValues[index] = editedValue;
       
+      // newValues2[index2] = editedValue2;
+      
       setNewRow2(newValues);
+
+      // setNewRow(newValues2);
   }
 
   const addRow = (e) => {
@@ -63,17 +102,17 @@ const App = () => {
     for (let i = 0; i < parseInt(row); i++) {
       row1[i] = allRowsAdded + i;
     }
+
     updateAllRows((allRowsAdded) => allRowsAdded + parseInt(row));
     setNewRow([...NewRow, ...row1]);
-
   };
 
-  const deleteRow = (id) => {
-    const updatedRows = [...NewRow].filter((rowId) => {
-      return rowId !== id;
-    });
-    setNewRow(updatedRows);
-  };
+  // const deleteRow = (id) => {
+  //   const updatedRows = [...NewRow].filter((rowId) => {
+  //     return rowId !== id;
+  //   });
+  //   setNewRow(updatedRows);
+  // };
 
   const deleteRow2 = (id) => {
     const updatedRows = [...NewRow2].filter((rowId) => {
@@ -82,9 +121,13 @@ const App = () => {
     setNewRow2(updatedRows);
   };
 
-  const demo = (id) => {
-    setIsRowId(id)
+  const setId = (id) => {
+    setIsRowId(id);
   }
+
+  // const setId2 = (id) => {
+  //   setIsRowId2(id);
+  // }
 
   return (
     <>
@@ -141,58 +184,46 @@ const App = () => {
                 return (
                   <tr key={rowId}>
 
-                    <td> <input type='text' className="form-control" defaultValue={element.Injection_Speed} name="Injection_Speed" onChange={handleEditFormChange} onClick={() => demo(rowId)} /> </td>
+                    <td> <input type='text' className="form-control" defaultValue={element.Injection_Speed} name="Injection_Speed" onChange={handleEditFormChange} onClick={() => setId(rowId)} /> </td>
 
-                    <td> <input type='text' className="form-control" defaultValue={element.Fill_Time} name="Fill_Time" onChange={handleEditFormChange} onClick={() => demo(rowId)}/></td>
+                    <td> <input type='text' className="form-control" defaultValue={element.Fill_Time} name="Fill_Time" onChange={handleEditFormChange} onClick={() => setId(rowId)} /></td>
 
-                    <td><input type='text' className="form-control" defaultValue={element.Peak_Inj_Press} name="Peak_Inj_Press" onChange={handleEditFormChange} onClick={() => demo(rowId)}/> </td>
+                    <td><input type='text' className="form-control" defaultValue={element.Peak_Inj_Press} name="Peak_Inj_Press" onChange={handleEditFormChange} onClick={() => setId(rowId)} /> </td>
 
-                    <td> <input type='text' className="form-control" name="Viscosity" value={isNaN(Math.round(element.Viscosity)) ? '-' : Math.round(element.Viscosity) } onChange={handleEditFormChange} onClick={() => demo(rowId)} readOnly/> </td>
+                    <td> <input type='text' className="form-control" name="Viscosity" value={isNaN(Math.round(element.Viscosity)) ? '-' : Math.round(element.Viscosity)} readOnly/> </td>
 
-                    <td>  <input type='text' className="form-control" name="Shear_Rate" value={isNaN(Number(element.Shear_Rate).toFixed(3)) ? '-' : Number(element.Shear_Rate).toFixed(3)} readOnly /> </td>
+                    <td>  <input type='text' className="form-control" name="Shear_Rate" value={isNaN(Number(element.Shear_Rate)) ? '-' : Number(element.Shear_Rate).toFixed(3)} readOnly/> </td>
 
-                    <td> <input type='text' className="form-control" readOnly /></td>
+                    <td> <input type='text' name="Absolute_Viscosity" value={ rowId === 0 ? '-' : (isNaN( Math.round(NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity) ) ? '-' : Math.round(NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity) )} className="form-control" readOnly/></td>
 
-                    <td> <input type='text' className="form-control" readOnly /></td>
+                    <td> <input type='text' name="Drop_Viscosity" value={ rowId === 0 ? '-' : (isNaN(Number( ( ( NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity ) * 100) / ( NewRow2[rowId - 1].Viscosity ) ) ) ? '-' : ( Number( ( ( NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity ) * 100) / ( NewRow2[rowId - 1].Viscosity ) ) ).toFixed(1) ) } className="form-control" readOnly/></td>
 
                     <td> <i className="fa fa-trash viscocity_icons" onClick={() => deleteRow2(element)}></i> </td>
                   </tr>
                 )
               })}
-              {NewRow.map((rowId) => {
-              return (
-                <tr key={rowId}>
-                  <td>
-                    <input type="text" className="form-control" />
-                  </td>
-                  <td>
-                    <input type="text" className="form-control" />
-                  </td>
-                  <td>
-                    <input type="text" className="form-control" />
-                  </td>
-                  <td>
-                    <input type="text" className="form-control" readOnly />
-                  </td>
-                  <td>
-                    <input type="text" className="form-control" readOnly />
-                  </td>
-                  <td>
-                    <input type="text" className="form-control" readOnly />
-                  </td>
-                  <td>
-                    <input type="text" className="form-control" readOnly />
-                  </td>
-                  <td>
-                    <i
-                      className="fa fa-trash viscocity_icons"
-                      onClick={() => deleteRow(rowId)}
-                    >
-                    </i>
-                  </td>
-                </tr>
-              );
-            })}
+              {/* {NewRow.map((element, rowId) => {
+                return (
+                  <tr key={rowId}>
+
+                    <td> <input type='text' className="form-control" defaultValue={element.Injection_Speed2} name="Injection_Speed2" onChange={handleEditFormChange2} onClick={() => setId2(rowId)} /> </td>
+
+                    <td> <input type='text' className="form-control" defaultValue={element.Fill_Time2} name="Fill_Time2" onChange={handleEditFormChange2} onClick={() => setId2(rowId)} /></td>
+
+                    <td><input type='text' className="form-control" defaultValue={element.Peak_Inj_Press2} name="Peak_Inj_Press2" onChange={handleEditFormChange2} onClick={() => setId2(rowId)} /> </td>
+
+                    <td> <input type='text' className="form-control" name="Viscosity2" value={isNaN(Math.round(element.Viscosity2)) ? '-' : Math.round(element.Viscosity2)} readOnly/> </td>
+
+                    <td>  <input type='text' className="form-control" name="Shear_Rate2" value={isNaN(Number(element.Shear_Rate2)) ? '-' : Number(element.Shear_Rate2).toFixed(3)} readOnly/> </td>
+
+                    <td> <input type='text' name="Absolute_Viscosity2" value={ rowId === 0 ? '-' : (isNaN( Math.round(NewRow[rowId - 1].Viscosity2 - NewRow[rowId].Viscosity2) ) ? '-' : Math.round(NewRow[rowId - 1].Viscosity2 - NewRow[rowId].Viscosity2) )} className="form-control" readOnly/></td>
+
+                    <td> <input type='text' name="Drop_Viscosity2" value={ rowId === 0 ? '-' : (isNaN(Number( ( ( NewRow[rowId - 1].Viscosity2 - NewRow[rowId].Viscosity2 ) * 100) / ( NewRow[rowId - 1].Viscosity2 ) ) ) ? '-' : ( Number( ( ( NewRow[rowId - 1].Viscosity2 - NewRow[rowId].Viscosity2 ) * 100) / ( NewRow[rowId - 1].Viscosity2 ) ) ).toFixed(1) ) } className="form-control" readOnly/></td>
+
+                    <td> <i className="fa fa-trash viscocity_icons" onClick={() => deleteRow(element)}></i> </td>
+                  </tr>
+                )
+              })} */}
             </tbody>
           </Table>
           <button type="submit"> Calculate </button>
