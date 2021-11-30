e67f8c532772c3884c1afd800a9ece4bf10c2ea3
Suraj Vishwakarma <vishwakarmasuraj631@gmail.com>
2021-11-30 11:34:33 +0530

    new

--- src/App.js
+++ src/App.js
@@ -101,17 +101,12 @@ const App = () => {
         </form>
         <button onClick={increaseRow}> Add </button>
 
-
       </div>
       <div className="container">
-        <form onSubmit={handleEditFormSubmit} >
+        <form onSubmit={handleEditFormSubmit}>
           <Table striped bordered hover responsive variant="light">
             <thead>
               <tr>
-                <th>
-                  {" "}
-                  <h6> No. </h6>{" "}
-                </th>
                 <th>
                   {" "}
                   <h6> Injection Speed </h6>{" "}
@@ -151,23 +146,26 @@ const App = () => {
                 return (
                   <tr key={rowId} onClick={() => setId(rowId)}>
 
-                    <td> {rowId } </td>
+                    <td> <input type='text' className="form-control" value={NewRow2[rowId].Injection_Speed} name="Injection_Speed" onChange={handleEditFormChange} /> </td>
 
-                    <td> <input type='text' className="form-control" defaultValue={NewRow2[rowId].Injection_Speed} name="Injection_Speed" onChange={handleEditFormChange} /> </td>
+                    <td> <input type='text' className="form-control" value={NewRow2[rowId].Fill_Time} name="Fill_Time" onChange={handleEditFormChange} /> </td>
 
-                    <td> <input type='text' className="form-control" defaultValue={NewRow2[rowId].Fill_Time} name="Fill_Time" onChange={handleEditFormChange} /></td>
+                    <td><input type='text' className="form-control" value={NewRow2[rowId].Peak_Inj_Press} name="Peak_Inj_Press" onChange={handleEditFormChange} /> </td>
 
-                    <td><input type='text' className="form-control" defaultValue={NewRow2[rowId].Peak_Inj_Press} name="Peak_Inj_Press" onChange={handleEditFormChange} /> </td>
-
-                    <td> <input type='text' className="form-control" name="Viscosity" value={isNaN(Math.round(element.Viscosity)) ? '-' : Math.round(element.Viscosity)} readOnly /> </td>
+                    <td> <input type='text' className="form-control" name="Viscosity" value={isNaN(Math.round(NewRow2[rowId].Viscosity)) ? '-' : Math.round(NewRow2[rowId].Viscosity)} readOnly /> </td>
 
                     <td>  <input type='text' className="form-control" name="Shear_Rate" value={isNaN(Number(element.Shear_Rate)) ? '-' : Number(element.Shear_Rate).toFixed(3)} readOnly /> </td>
 
-                    <td> <input type='text' name="Absolute_Viscosity" value={rowId === 0 ? '-' : (isNaN(Math.round(NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity)) ? '-' : Math.round(NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity))} className="form-control" readOnly /></td>
+                    <td> <input type='text' name="Absolute_Viscosity" value={rowId === 0 ? '-' : (isNaN(Math.round(NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity)) ? '-' : Math.round(NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity))} className="form-control" readOnly />
+                    </td>
 
-                    <td> <input type='text' name="Drop_Viscosity" value={rowId === 0 ? '-' : (isNaN(Number(((NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity) * 100) / (NewRow2[rowId - 1].Viscosity))) ? '-' : (Number(((NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity) * 100) / (NewRow2[rowId - 1].Viscosity))).toFixed(1))} className="form-control" readOnly /></td>
+                    <td> <input type='text' name="Drop_Viscosity" value={rowId === 0 ? '-' : (isNaN(Number(((NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity) * 100) / (NewRow2[rowId - 1].Viscosity))) ? '-' : (Number(((NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity) * 100) / (NewRow2[rowId - 1].Viscosity))).toFixed(1))} className="form-control" readOnly />
+                    </td>
 
-                    <td> <i className="fa fa-trash viscocity_icons" onClick={() => deleteRow2(element)}></i> </td>
+                    <td> 
+                      <i className="fa fa-edit viscocity_icons"></i> 
+                      <i className="fa fa-trash viscocity_icons" onClick={() => deleteRow2(element)}></i> 
+                    </td>
                   </tr>
                 )
               })}
