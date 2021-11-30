99c5db531d12a5ec85fc71f4d5d31a9e84a6fc89
Suraj Vishwakarma <vishwakarmasuraj631@gmail.com>
2021-11-30 15:34:19 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Table from "react-bootstrap/Table";
 import './App.css'
-import ReadOnly from "./ReadOnly";
 
 const App = () => {
   const row1 = [];
@@ -69,25 +68,25 @@ const App = () => {
     setNewRow2([...NewRow2, ...row1]);
   };
 
-  const deleteRow2 = (id) => {
-    const updatedRows = [...NewRow2].filter((rowId) => {
-      return rowId !== id;
-    });
-    setNewRow2(updatedRows);
-  };
-
   // const deleteRow2 = (id) => {
-
-  //   const newValues = [...NewRow2];
-
-  //   const index = NewRow2.findIndex((value) => value.id === id);
-
-  //   newValues.splice(index, 1);
-
-  //   setNewRow2(newValues);
-
-  //   console.log(id);
+  //   const updatedRows = [...NewRow2].filter((rowId) => {
+  //     return rowId !== id;
+  //   });
+  //   setNewRow2(updatedRows);
   // };
+
+  const deleteRow2 = (id) => {
+
+    const newValues = [...NewRow2];
+
+    const index = NewRow2.findIndex((value) => value === id);
+
+    newValues.splice(index, 1);
+
+    setNewRow2(newValues);
+
+    console.log(id);
+  };
 
   const setId = (id) => {
     setIsRowId(id);
@@ -98,21 +97,16 @@ const App = () => {
       <div>
         <form>
           <input type="text" onChange={addRow} placeholder="Enter Number Of Row's" /><br />
-          <input type="text" onClick={(e) => setIntensificationRatio(e.target.value)} placeholder="Enter Number Of Row's" /><br />
+          <input type="text" onChange={(e) => setIntensificationRatio(e.target.value)} placeholder="Enter Intensification Ratio" />
         </form>
         <button onClick={increaseRow}> Add </button>
 
-
       </div>
       <div className="container">
-        <form onSubmit={handleEditFormSubmit} >
+        <form onSubmit={handleEditFormSubmit}>
           <Table striped bordered hover responsive variant="light">
             <thead>
               <tr>
-                <th>
-                  {" "}
-                  <h6> No. </h6>{" "}
-                </th>
                 <th>
                   {" "}
                   <h6> Injection Speed </h6>{" "}
@@ -150,13 +144,29 @@ const App = () => {
             <tbody className="grid_style">
               {NewRow2.map((element, rowId) => {
                 return (
-                  <ReadOnly NewRow2={NewRow2} element={element} rowId={rowId} setId={setId} handleEditFormChange={handleEditFormChange} deleteRow2={deleteRow2} />
+                  <tr key={rowId} onClick={() => setId(rowId)}>
+
+                    <td> <input type='text' className="form-control" value={NewRow2[rowId].Injection_Speed} name="Injection_Speed" onChange={handleEditFormChange} /> </td>
+
+                    <td> <input type='text' className="form-control" value={NewRow2[rowId].Fill_Time} name="Fill_Time" onChange={handleEditFormChange} /> </td>
+
+                    <td><input type='text' className="form-control" value={NewRow2[rowId].Peak_Inj_Press} name="Peak_Inj_Press" onChange={handleEditFormChange} /> </td>
+
+                    <td> <input type='text' className="form-control" name="Viscosity" value={isNaN(Math.round(NewRow2[rowId].Viscosity)) ? '-' : Math.round(element.Viscosity)} readOnly /> </td>
+
+                    <td>  <input type='text' className="form-control" name="Shear_Rate" value={isNaN(Number(element.Shear_Rate)) ? '-' : Number(element.Shear_Rate).toFixed(3)} readOnly /> </td>
+
+                    <td> <input type='text' name="Absolute_Viscosity" value={rowId === 0 ? '-' : (isNaN(Math.round(NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity)) ? '-' : Math.round(NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity))} className="form-control" readOnly /></td>
+
+                    <td> <input type='text' name="Drop_Viscosity" value={rowId === 0 ? '-' : (isNaN(Number(((NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity) * 100) / (NewRow2[rowId - 1].Viscosity))) ? '-' : (Number(((NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity) * 100) / (NewRow2[rowId - 1].Viscosity))).toFixed(1))} className="form-control" readOnly /></td>
+
+                    <td> <i className="fa fa-trash viscocity_icons" onClick={() => deleteRow2(element)}></i> </td>
+                  </tr>
                 )
               })}
             </tbody>
           </Table>
           <button type="submit" className="mt-4"> Calculate </button>
-
         </form>
       </div>
     </>
