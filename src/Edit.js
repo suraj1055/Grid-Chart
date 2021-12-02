930bfc1d02196ff058c9d50852c2d7165d66a5de
Suraj Vishwakarma <vishwakarmasuraj631@gmail.com>
2021-12-02 17:45:29 +0530

    ..

--- src/Edit.js
+++ src/Edit.js
@@ -14,9 +14,9 @@ const edit = ({ setId, NewRow2, handleEditFormChange, deleteRow2, NewRow, rowId 
 
             <td> <input type='text' className="form-control" name="Shear_Rate" value={ NewRow2[rowId].Shear_Rate === "" ? '-' : Number(NewRow.Shear_Rate).toFixed(3)} readOnly/> </td>
 
-            <td> <input type='text' className="form-control" name="Absolute_Viscosity" value={ NewRow.id === 0 ? ('-') : ( NewRow2[rowId ].Viscosity === "" ? '-' : ( Math.round(NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity) ) ) } readOnly/> </td>
+            <td> <input type='text' className="form-control" name="Absolute_Viscosity" value={ rowId === 0 ? ('-') : ( NewRow2[rowId].Viscosity === "" ? '-' : ( Math.round(NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity) ) ) } readOnly/> </td>
 
-            <td> <input type='text' className="form-control" name="Drop_Viscosity" value={ NewRow.id === 0 ? ('-') : (  NewRow2[rowId ].Viscosity === "" ? '-' : Number( (Math.round(NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity) * 100 ) / ( NewRow2[rowId - 1].Viscosity ) ).toFixed(1) ) } readOnly/> </td>
+            <td> <input type='text' className="form-control" name="Drop_Viscosity" value={ rowId === 0 ? ('-') : (  NewRow2[rowId].Viscosity === "" ? '-' : Number( (Math.round(NewRow2[rowId - 1].Viscosity - NewRow2[rowId].Viscosity) * 100 ) / ( NewRow2[rowId - 1].Viscosity ) ).toFixed(1) ) } readOnly/> </td>
 
             <td> <i className="fa fa-trash viscocity_icons" onClick={() => deleteRow2(NewRow)}></i> </td>
         </tr>
